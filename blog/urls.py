b432ca94444e911f627847c1e338c32675a60806
Serge <zavizionsergey@gmail.com>
2018-07-31 17:56:54 +0300

    delete comments

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    # path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/create/', views.ArticleCreate.as_view(), name='post_create'),
 ]
