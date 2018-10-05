7a397508eed10b6098855a70bbc1e5d7750a839c
Serge <zavizionsergey@gmail.com>
2018-10-05 17:21:13 +0300

    update urls

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('blog.accounts.urls'))
+    path('registration/', include('blog.accounts.urls'))
 ]
