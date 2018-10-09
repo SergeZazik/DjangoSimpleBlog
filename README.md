9292293c2a9baae80dc959e787e6c8baf6c3f644
Serge <zavizionsergey@gmail.com>
2018-10-09 12:51:10 +0300

    update login and registration templates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DjangoSimpleBlog
+
+## Setup
+
+1. `python3 -m venv venv`
+2. `source venv/bin/activate`
+3. `pip install -r requirements.txt`
+4. `python manage.py migrate`
--- blog/accounts/forms.py
+++ blog/accounts/forms.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
